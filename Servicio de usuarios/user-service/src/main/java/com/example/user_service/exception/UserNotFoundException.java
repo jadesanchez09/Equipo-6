8617a92dc129c4d47abfedda441659bb9333a183
jadesanchez09 <jade.sanchez3522@alumnos.udg.mx>
2026-03-22 23:21:13 -0600

    .

--- Servicio de usuarios/user-service/src/main/java/com/example/user_service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("Usuario no encontrado con id: " + id);
-    }
-}
--- Servicio de usuarios/user-service/src/main/java/com/example/user_service/exception/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.user_service.exception;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String username) {
-        super("El username ya está registrado: " + username);
-    }
-}
